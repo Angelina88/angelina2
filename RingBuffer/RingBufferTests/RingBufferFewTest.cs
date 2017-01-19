b8b6319e94c90601391d47b41c66195421949101
a.obolonina <angelinka.lav@gmail.com>
2017-01-19 16:16:53 +0300

    ring buffer

---
+++ RingBuffer/RingBufferTests/RingBufferFewTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingBuffer;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+
+namespace RingBufferTests
+{
+    /// <summary>
+    /// Сводное описание для RingBufferFewTest
+    /// </summary>
+    [TestClass]
+    public class RingBufferFewTest
+    {
+        private IBuffer<byte> _buffer;
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            _buffer = new RingBuffer<byte>(64, 8);
+        }
+
+        [TestMethod]
+        public void AsyncPushTest()
+        {
+            var item1 = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 };
+            var item2 = new byte[] { 2, 2, 2, 2, 2, 2, 2, 2 };
+            var item3 = new byte[] { 3, 3, 3, 3, 3, 3, 3, 3 };
+            var task1 = Task.Factory.StartNew(
+                () => {
+                    Thread.Sleep(100);
+                    _buffer.Push(new List<byte[]> { item1 });
+                });
+            var task2 = Task.Factory.StartNew(
+                () => {
+                    _buffer.Push(new List<byte[]> { item2 });
+                    Thread.Sleep(100);
+                    _buffer.Push(new List<byte[]> { item3 });
+                });
+            Task.WaitAll(task1, task2);
+
+            var resultItems = _buffer.Pop(3);
+
+            Assert.AreEqual(3, resultItems.Count());
+        }
+
+        [TestMethod]
+        public void AsyncPushAndPopTest()
+        {
+            var item1 = new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 };
+            var item2 = new byte[] { 2, 2, 2, 2, 2, 2, 2, 2 };
+            var item3 = new byte[] { 3, 3, 3, 3, 3, 3, 3, 3 };
+            var task1 = Task.Factory.StartNew(
+                () => {
+                    Thread.Sleep(100);
+                    _buffer.Push(new List<byte[]> { item1 });
+                    Thread.Sleep(1000);
+                    _buffer.Pop(1);
+                });
+            var task2 = Task.Factory.StartNew(
+                () => {
+                    _buffer.Push(new List<byte[]> { item2 });
+                    Thread.Sleep(100);
+                    _buffer.Push(new List<byte[]> { item3 });
+                    Thread.Sleep(500);
+                    _buffer.Pop(2);
+                });
+            var task3 = Task.Factory.StartNew(
+                () => {
+                    Thread.Sleep(1000);
+                    _buffer.Push(new List<byte[]> { item1 });
+                    Thread.Sleep(100);
+                    _buffer.Push(new List<byte[]> { item1 });
+                    _buffer.Pop(2);
+                });
+            Task.WaitAll(task1, task2, task3);
+
+            var resultItems = _buffer.Pop(3);
+
+            Assert.AreEqual(0, resultItems.Count());
+        }
+    }
+}
---
+++ RingBuffer/RingBufferTests/RingBufferSingleTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingBuffer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RingBufferTests
+{
+    [TestClass]
+    public class RingBufferSingleTest
+    {
+        [TestMethod]
+        public void PushTest()
+        {
+            var buffer = new RingBuffer<byte>(32, 8);
+
+            var count = buffer.Push(new List<byte[]>() { new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 } });
+
+            Assert.AreEqual(0, buffer.Head);
+            Assert.AreEqual(8, buffer.End);
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            var buffer = new RingBuffer<byte>(32, 8);
+            var item = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            buffer.Push(new List<byte[]>() { item });
+
+            var popItems = buffer.Pop(1);
+
+            Assert.AreEqual(8, buffer.Head);
+            Assert.AreEqual(8, buffer.End);
+            Assert.AreEqual(1, popItems.Count());
+            for (var i = 0; i < item.Length; i++)
+                Assert.AreEqual(item[i], popItems.First()[i]);
+        }
+
+        [TestMethod]
+        public void PushOverflowTest()
+        {
+            var buffer = new RingBuffer<byte>(32, 8);
+
+            var count = buffer.Push(new List<byte[]>()
+                                    {
+                                        new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                                        new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                                        new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                                        new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                                        new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 }
+                                    });
+
+            Assert.AreEqual(0, buffer.Head);
+            Assert.AreEqual(0, buffer.End);
+            Assert.AreEqual(4, count);
+        }
+
+        [TestMethod]
+        public void PopEmptyTest()
+        {
+            var buffer = new RingBuffer<byte>(32, 8);
+            buffer.Push(new List<byte[]>());
+
+            var popItems = buffer.Pop(1);
+
+            Assert.AreEqual(0, buffer.Head);
+            Assert.AreEqual(0, buffer.End);
+            Assert.AreEqual(0, popItems.Count());
+
+        }
+
+        [TestMethod]
+        public void PushAndPopTest()
+        {
+            var buffer = new RingBuffer<byte>(32, 8);
+
+            var item = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var count = buffer.Push(new List<byte[]>() { item, item, item, item });
+
+            var popItems = buffer.Pop(2);
+
+            //Пересечение элеменами границы 0
+            buffer.Push(new[] { item });
+
+            Assert.AreEqual(16, buffer.Head);
+            Assert.AreEqual(8, buffer.End);
+
+            //Полное заполнение буфера на отметке отлично от 0
+            count = buffer.Push(new[] { item, item });
+
+            Assert.AreEqual(1, count);
+            Assert.AreEqual(16, buffer.End);
+
+            //Полное извлечение из буфера
+            buffer.Pop(4);
+            Assert.AreEqual(16, buffer.Head);
+            Assert.AreEqual(16, buffer.End);
+
+            //Перезаполнение буфера
+            count = buffer.Push(new[] { item, item });
+
+            Assert.AreEqual(2, count);
+            Assert.AreEqual(0, buffer.End);
+
+            buffer.Pop(2);
+            Assert.AreEqual(0, buffer.Head);
+        }
+    }
+}
